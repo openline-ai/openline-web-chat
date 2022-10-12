e899f38ae9b9a89f95a0137e88506572b9220da9
gabrielgontariu <gabi@openline.ai>
2022-10-12 14:40:25 +0300

    avatar

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,33 @@
+import {CSSProperties} from "react";
+
+export const styles: { [key: string]: CSSProperties } = {
+    chatWithOpenlineButton: {
+        cursor: 'pointer',
+        boxShadow: '0px 0px 16px 6px rgba(0, 0, 0, 0.33)',
+        // Border
+        borderRadius: '50%',
+        // Background
+        backgroundImage: `url(/teamLogo.png)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '84px',
+        // Size
+        width: '84px',
+        height: '84px',
+    },
+    avatarHello: {
+        // Position
+        position: 'absolute',
+        left: 'calc(-100% - 44px - 28px)',
+        top: 'calc(50% - 24px)',
+        // Layering
+        zIndex: '10000',
+        boxShadow: '0px 0px 16px 6px rgba(0, 0, 0, 0.33)',
+        // Border
+        padding: '12px 12px 12px 16px',
+        borderRadius: '24px',
+        // Color
+        backgroundColor: '#f9f0ff',
+        color: 'black',
+    }
+}
