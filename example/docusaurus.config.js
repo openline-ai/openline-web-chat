87ddcc2bc49c0ca6e2da83eadc045f8eeace27ce
Gabriel Gontariu <gabi@openline.ai>
2022-11-29 08:50:30 +0200

    19-fix-docusaurus

--- example/docusaurus.config.js
+++ example/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
+  plugins: ['docusaurus-node-polyfills'],
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
