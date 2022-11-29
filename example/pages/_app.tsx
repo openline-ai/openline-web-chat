23831c0cb3e59a8b9954e0b2687d61d4522d6ed4
Gabriel Gontariu <gabi@openline.ai>
2022-11-29 07:39:50 +0200

    17-fix-docusaurus

--- example/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'
-import '@openline-ai/openline-web-chat/dist/esm/index.css'
-import type {AppProps} from 'next/app'
-
-export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
-}
