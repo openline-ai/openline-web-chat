bd2f6318fee2aaa5bd7786eb5b1c534fc19c2a4d
Gabriel Gontariu <gabi@openline.ai>
2022-11-29 10:08:01 +0200

    21-example-css-fixes

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import '@openline-ai/openline-web-chat/dist/esm/index.css'
 import styles from './index.module.css';
 import {WebChat} from "@openline-ai/openline-web-chat";
 
